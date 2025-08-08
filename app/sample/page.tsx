c46591848d2ec094e69d45e3af997ac5ef79663b
unknown <Carlos Isaac R. Sison>
2025-08-08 10:13:46 +0800

    sample again

--- app/sample/page.tsx
+++ app/sample/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Page() {
   React.useEffect(() => {
     (async () => {
-      const data = await fetch('api/', { cache: 'no-store' });
+      const data = await fetch('/api', { cache: 'no-store' });
 
       console.log(await data.json());
     })();

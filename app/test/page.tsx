6ddb5ca3f984fec2588783b0a4af590dc4b4531b
unknown <Carlos Isaac R. Sison>
2025-08-08 10:17:58 +0800

    server api

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,10 @@
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  const data = await fetch('http://localhost:3212/api/', { cache: 'no-store' });
+  const data = await fetch(
+    'https://sample-qzw5pm2oe-carlossison75s-projects.vercel.app/api/',
+    { cache: 'no-store' }
+  );
 
   console.log(await data.json());
 

6c1b3edc9a9f7aed4cb3c3bdb645082ffa1c67a6
unknown <Carlos Isaac R. Sison>
2025-08-08 11:40:43 +0800

    with try catch

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,16 +3,20 @@ import { headers } from 'next/headers';
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  const headersList = await headers();
-  const protocol = headersList.get('x-forwarded-proto') || 'http';
-  const host = headersList.get('host');
-  const baseUrl = `${protocol}://${host}`;
+  try {
+    const headersList = await headers();
+    const protocol = headersList.get('x-forwarded-proto') || 'http';
+    const host = headersList.get('host');
+    const baseUrl = `${protocol}://${host}`;
 
-  console.log(baseUrl);
+    console.log(baseUrl);
 
-  const data = await fetch(`${baseUrl}/api`, { cache: 'no-store' });
+    const data = await fetch(`${baseUrl}/api`, { cache: 'no-store' });
 
-  console.log(await data.json());
+    console.log(await data.json());
+  } catch (error) {
+    console.log('JSON ERROR: ', error);
+  }
 
   return <div>Page</div>;
 }

b76f50e90240b93c617cb8bdeaecb8aee708f5b6
unknown <Carlos Isaac R. Sison>
2025-08-08 10:14:31 +0800

    sample again

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,7 @@
-import PUBLIC_URL from '@/constants';
-
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
-  const data = await fetch(PUBLIC_URL, { cache: 'no-store' });
+  const data = await fetch('http://localhost:3212/api/', { cache: 'no-store' });
 
   console.log(await data.json());
 

e1eb803d3866eea4f8043e25000b4b0a174fa80a
unknown <Carlos Isaac R. Sison>
2025-08-08 11:36:57 +0800

    width log

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -7,6 +7,9 @@ export default async function Page() {
   const protocol = headersList.get('x-forwarded-proto') || 'http';
   const host = headersList.get('host');
   const baseUrl = `${protocol}://${host}`;
+
+  console.log(baseUrl);
+
   const data = await fetch(`${baseUrl}/api`, { cache: 'no-store' });
 
   console.log(await data.json());

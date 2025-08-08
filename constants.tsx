45391022017c2231bddc40c30584a2be15d1ed5d
unknown <Carlos Isaac R. Sison>
2025-08-08 11:21:56 +0800

    removed -p

--- constants.tsx
+++ constants.tsx
@@ -1,7 +1,7 @@
 const PUBLIC_URL = process.env.API_PATH;
 
 if (!PUBLIC_URL) {
-  throw new Error('NEXT_PUBLIC_API_PATH is not defined');
+  throw new Error('API_PATH is not defined');
 }
 
 export default PUBLIC_URL;
